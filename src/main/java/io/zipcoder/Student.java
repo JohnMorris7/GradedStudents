a1b6a184c91e4498e894f8b863d6d1095e52bc25
John <john@Zipcoders-MacBook-Pro-46.local>
2022-11-18 23:15:12 -0500

    got a little done

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,19 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+
+    String firstName;
+    String lastName;
+    ArrayList<Double> examScores;
+    public Student(String firstName, String lastName, ArrayList<Double> examScores) {
+        public String getFirstName() {
+            return firstName;
+        }
+        public void setFirstName(String newFirstName) {
+            this.firstName = newFirstName;
+        }
+    }
 }
+
