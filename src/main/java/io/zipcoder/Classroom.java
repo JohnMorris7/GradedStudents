584153dc22422573e53e50faa840551e2ca62145
John <john@Zipcoders-MacBook-Pro-46.local>
2022-11-22 23:30:42 -0500

    did a little bit of stuff

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -2,4 +2,19 @@ package io.zipcoder;
 
 public class Classroom {
 
+    Student[] students;
+
+    public Classroom(int maxNumberOfStudents) {
+    }
+    public Classroom(Student[] students) {
+    }
+    public Classroom() {
+        Student[] students = new Student[30];
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+
 }
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -4,16 +4,66 @@ import java.util.ArrayList;
 
 public class Student {
 
-    String firstName;
-    String lastName;
-    ArrayList<Double> examScores;
-    public Student(String firstName, String lastName, ArrayList<Double> examScores) {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+    public Student(String firstName, String lastName, ArrayList<Double> testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = testScores;
+        }
+
         public String getFirstName() {
             return firstName;
         }
-        public void setFirstName(String newFirstName) {
-            this.firstName = newFirstName;
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public int getNumberOfExamsTaken() {
+            return examScores.size();
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+        public String getExamScores() {
+            String output = "Exam Scores:" + "\n";
+            System.out.println("Exam Scores:" + "\n");
+            for (int i = 0; i < examScores.size(); i++) {
+                output += "Exam " + (i + 1) + " -> " + examScores.get(i);
+                System.out.println("Exam " + (i + 1) + " -> " + examScores.get(i));
+            }
+            return output;
+        }
+
+        public void addExamScore(double examScore) {
+            examScores.add(examScore);
+        }
+
+        public void setExamScore(int examNumber, double newScore) {
+            examScores.set(examNumber - 1, newScore);
+        }
+        public double getAverageExamScore() {
+            Double total = 0.0;
+            for (int i = 0; i < examScores.size(); i++) {
+                total += examScores.get(i);
+            }
+            Double average = total / (examScores.size() - 1);
+            return average;
+        }
+
+        @Override
+        public String toString() {
+            return "Student Name:  " + firstName + " "  + lastName  +
+                    "\n > Average Score: " + getAverageExamScore()
+                    +"\n >" + getExamScores();
         }
     }
-}
+
 
